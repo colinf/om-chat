578a849b7629664452d88833d160cf46425afa0f
colinf <colinf@mac.com>
2015-12-10 15:00:26 +0000

    Upgrade to om alpha26

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"]
-                 [org.omcljs/om "1.0.0-alpha24"]
+                 [org.omcljs/om "1.0.0-alpha26"]
                  [figwheel-sidecar "0.5.0-2" :scope "test"]
                  [com.andrewmcveigh/cljs-time "0.3.14"]]
   )
